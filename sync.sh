7bf602e7ed0e5c57bfe791f2aa25a1bddd2586c0
Ameer Dawood <ameer1234567890@gmail.com>
2018-08-22 14:46:29 +0500

    Modernize

--- sync.sh
+++ sync.sh
@@ -1,1 +1,2 @@
-rsync -av --delete --progress --backup --backup-dir="/Phone.old/backup_$(date +\%Y-\%m-\%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.7.1/usb1/Phone
+#!/bin/sh
+rsync -av --delete --progress --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.7.1/usb1/Phone
