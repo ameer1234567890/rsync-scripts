a7fe57089fd583c4667e6d31f868d3d72be821ba
Ameer Dawood <ameer1234567890@gmail.com>
2018-09-14 19:13:37 +0500

    Humanize

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -av --delete --progress --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.7.1/usb1/Phone
+rsync -ahv --delete --progress --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.7.1/usb1/Phone
--- syncin.sh
+++ syncin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -av --delete --progress --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt rsync://user@192.168.7.1/usb1/Phone/ /storage/emulated/0
+rsync -ahv --delete --progress --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt rsync://user@192.168.7.1/usb1/Phone/ /storage/emulated/0
