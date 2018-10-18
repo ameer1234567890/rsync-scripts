f43111857e9e03f6c7f416c16d5e37b93774eace
Ameer Dawood <ameer1234567890@gmail.com>
2018-10-18 18:22:55 +0500

    Add closing pause

--- sync.sh
+++ sync.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 rsync -ahv --delete --progress --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.7.1/usb1/Phone
+printf "Press enter to exit..."
+read -r
