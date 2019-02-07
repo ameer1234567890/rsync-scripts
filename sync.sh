1d1145acc34f154f839f049529bcf93d261d09ea
Ameer Dawood <ameer1234567890@gmail.com>
2019-02-07 17:16:36 +0500

    Use Termux notification api

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 rsync -ahv --delete --progress --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.7.1/usb1/Phone
-printf "Press enter to exit..."
-read -r
+status="$?"
+if [ "$status" != 0 ]; then
+  termux-notification --title "Error!" --content "An error occured during backup!"
+else
+  termux-notification --title "Success" --content "Backup successful"
+fi
