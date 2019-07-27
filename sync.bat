f954d615b2638ea63dd95d3a7111fa12bdbe120b
Ameer Dawood <ameer1234567890@gmail.com>
2019-07-27 13:30:45 +0500

    Fix

--- sync.bat
+++ sync.bat
@@ -4,14 +4,15 @@ SET STATUS=%ERRORLEVEL%
 IF %STATUS% EQU 0 ECHO "USB is not mounted at remote! Exiting..."
 IF %STATUS% NEQ 0 rsync -rhtv --delete --progress --partial --fuzzy --delete-delay --modify-window=5 --partial-dir=.rsync-partial --backup --backup-dir="/Laptop.old/backup_%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%" --exclude-from /cygdrive/d/Ameer/rsync-excludes.txt /cygdrive/d/ rsync://user@miwifimini.lan/usb1/Laptop
 SET STATUS=%ERRORLEVEL%
-IF %STATUS% EQU 0 GOTO SUCCESS
-IF %STATUS% NQU 0 GOTO ERROR
-PAUSE
+IF %STATUS% EQU 0 GOTO :SUCCESS
+IF %STATUS% NQU 0 GOTO :ERROR
 
 :SUCCESS
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='Backup completed successfully!'; $objNotifyIcon.Icon=[system.drawing.systemicons]::'Information'; $objNotifyIcon.BalloonTipTitle='Backup'; $objNotifyIcon.BalloonTipIcon='Info'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(5000);"
+PAUSE
 GOTO :eof
 
 :ERROR
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='An error occured during backup! Please try again later!'; $objNotifyIcon.Icon=[system.drawing.systemicons]::'Error'; $objNotifyIcon.BalloonTipTitle='Backup'; $objNotifyIcon.BalloonTipIcon='Error'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(5000);"
+PAUSE
 GOTO :eof
