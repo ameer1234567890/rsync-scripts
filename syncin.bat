be3c1d057d500b07c76398e8436223e8495ecbde
Ameer Dawood <ameer1234567890@gmail.com>
2019-07-27 13:32:09 +0500

    Fix

--- syncin.bat
+++ syncin.bat
@@ -2,10 +2,10 @@
 
 :menu
 CHOICE /C YN /M "Are you sure you want to sync data in:" 
-IF ERRORLEVEL 2 GOTO reset
-IF ERRORLEVEL 1 GOTO sync
+IF ERRORLEVEL 2 GOTO :RESET
+IF ERRORLEVEL 1 GOTO :SYNC
 
-:sync
+:SYNC
 rsync rsync://miwifimini.lan/usb1/USB_NOT_MOUNTED >nul 2>&1
 SET STATUS=%ERRORLEVEL%
 IF %STATUS% NEQ 0 rsync -rhtv --delete --progress --partial --partial-dir=.rsync-partial --no-perms --no-owner --no-group --modify-window=5 --exclude-from /cygdrive/d/Ameer/rsync-excludes.txt rsync://user@miwifimini.lan/usb1/Laptop/ /cygdrive/d
@@ -13,7 +13,7 @@ IF %STATUS% EQU 0 ECHO "USB is not mounted at remote! Exiting..."
 PAUSE
 GOTO :eof
 
-:reset
+:RESET
 ECHO "Exiting now!"
 PAUSE
 GOTO :eof
