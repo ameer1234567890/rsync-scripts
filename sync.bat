1ddcc130b2a7681a62765a631f33b4a94d06e889
Ameer Dawood <ameer1234567890@gmail.com>
2019-08-04 15:11:54 +0500

    Fine grained backups

--- sync.bat
+++ sync.bat
@@ -2,7 +2,7 @@
 rsync rsync://miwifimini.lan/usb1/USB_NOT_MOUNTED >nul 2>&1
 SET STATUS=%ERRORLEVEL%
 IF %STATUS% EQU 0 ECHO "USB is not mounted at remote! Exiting..."
-IF %STATUS% NEQ 0 rsync -rhtv --delete --progress --partial --fuzzy --delete-delay --modify-window=5 --partial-dir=.rsync-partial --backup --backup-dir="/Laptop.old/backup_%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%" --exclude-from /cygdrive/d/Ameer/rsync-excludes.txt /cygdrive/d/ rsync://user@miwifimini.lan/usb1/Laptop
+IF %STATUS% NEQ 0 rsync -rhtv --delete --progress --partial --fuzzy --delete-delay --modify-window=5 --partial-dir=.rsync-partial --backup --backup-dir="/Laptop.old/backup_%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%" --exclude-from /cygdrive/d/Ameer/rsync-excludes.txt /cygdrive/d/ rsync://user@miwifimini.lan/usb1/Laptop
 SET STATUS=%ERRORLEVEL%
 IF %STATUS% EQU 0 GOTO :SUCCESS
 IF %STATUS% NQU 0 GOTO :ERROR
--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@ ifttt_key='YOUR_IFTTT_WEBHOOK_KEY_HERE'
 rsync rsync://192.168.100.44/usb1/USB_NOT_MOUNTED >/dev/null 2>&1
 status="$?"
 if [ "$status" != 0 ]; then
-  rsync -ahv --delete --progress --partial --partial-dir=.rsync-partial --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.100.44/usb1/Phone
+  rsync -ahv --delete --progress --partial --partial-dir=.rsync-partial --backup --backup-dir="/Phone.old/backup_$(date +%Y-%m-%d_%H.%M.%S)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@192.168.100.44/usb1/Phone
   status="$?"
   if [ "$status" != 0 ]; then
     curl -s -X POST https://maker.ifttt.com/trigger/rsyc_event/with/key/$ifttt_key --data 'value1=An+error+occured+during+backup!'
