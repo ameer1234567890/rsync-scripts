a50653967dcacb3a6b3af1438a08240f6e4a85c2
Ameer Dawood <ameer1234567890@gmail.com>
2019-08-16 00:25:23 +0500

    Fix invalid character in batch labels

--- sync.bat
+++ sync.bat
@@ -3,32 +3,32 @@ SET HOST="miwifimini.lan"
 
 PING -w 3 %HOST% >nul 2>&1
 SET STATUS=%ERRORLEVEL%
-IF %STATUS% NEQ 0 GOTO :ERROR_NOSRV
+IF %STATUS% NEQ 0 GOTO :ERRNOSRV
 rsync rsync://%HOST%/usb1/USB_NOT_MOUNTED >nul 2>&1
 SET STATUS=%ERRORLEVEL%
-IF %STATUS% EQU 0 GOTO :ERROR_NOUSB
+IF %STATUS% EQU 0 GOTO :ERRNOUSB
 IF %STATUS% NEQ 0 rsync -rhtv --copy-links --delete --progress --stats --partial --fuzzy --delete-delay --modify-window=5 --partial-dir=.rsync-partial --backup --backup-dir="/Ameer/Laptop.old/backup_%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%" --exclude-from /cygdrive/d/Ameer/rsync-excludes.txt /cygdrive/d/ rsync://user@%HOST%/usb1/Ameer/Laptop
 SET STATUS=%ERRORLEVEL%
 IF %STATUS% EQU 0 GOTO :SUCCESS
-IF %STATUS% NQU 0 GOTO :ERROR_NOBKP
+IF %STATUS% NQU 0 GOTO :ERRNOBKP
 
 :SUCCESS
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='Backup completed successfully!'; $objNotifyIcon.Icon=[system.drawing.systemicons]::'Information'; $objNotifyIcon.BalloonTipTitle='Backup'; $objNotifyIcon.BalloonTipIcon='Info'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(5000);"
 PAUSE
 GOTO :eof
 
-:ERROR_NOBKP
+:ERRNOBKP
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='An error occured during backup! Please try again later!'; $objNotifyIcon.Icon=[system.drawing.systemicons]::'Error'; $objNotifyIcon.BalloonTipTitle='Backup'; $objNotifyIcon.BalloonTipIcon='Error'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(5000);"
 PAUSE
 GOTO :eof
 
-:ERROR_NOSRV
+:ERRNOSRV
 ECHO "Backup server unavailable! Exiting..."
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='Backup server unavailable! Please try again later!'; $objNotifyIcon.Icon=[system.drawing.systemicons]::'Error'; $objNotifyIcon.BalloonTipTitle='Backup'; $objNotifyIcon.BalloonTipIcon='Error'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(5000);"
 PAUSE
 GOTO :eof
 
-:ERROR_NOUSB
+:ERRNOUSB
 ECHO "USB is not mounted at remote! Exiting..."
 powershell -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $objNotifyIcon=New-Object System.Windows.Forms.NotifyIcon; $objNotifyIcon.BalloonTipText='USB is not mounted at remote! Please try again later!'; $objNotifyIcon.Icon=[system.drawing.systemicons]::'Error'; $objNotifyIcon.BalloonTipTitle='Backup'; $objNotifyIcon.BalloonTipIcon='Error'; $objNotifyIcon.Visible=$True; $objNotifyIcon.ShowBalloonTip(5000);"
 PAUSE
