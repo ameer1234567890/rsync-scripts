becc0e14188d7de76baa0de5a1f319702541f56d
Ameer Dawood <ameer1234567890@gmail.com>
2019-08-26 13:36:59 +0500

    Local notifications instead of IFTTT

--- sync.sh
+++ sync.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 host="192.168.100.44"
-ifttt_key="YOUR_IFTTT_WEBHOOK_KEY_HERE"
 logger="/system/bin/log -t sync"
+
+log() {
+  echo "$@"
+  $logger -p e "$@"
+  termux-notification --title 'rsync' --content "$@"
+}
 
 ping -w 3 -c 1 $host >/dev/null 2>&1
 status="$?"
 if [ "$status" != 0 ]; then
-  echo "Server unavailable!"
-  $logger -p e "Server unavailable!"
-  curl -s -X POST https://maker.ifttt.com/trigger/rsync_event/with/key/$ifttt_key --data 'value1=Server+unavailable!' >/dev/null 2>&1
+  log "Server unavailable!"
 else
   rsync rsync://$host/usb1/USB_NOT_MOUNTED >/dev/null 2>&1
   status="$?"
@@ -16,17 +19,11 @@ else
     rsync -ahv --copy-links --delete --progress --stats --partial --partial-dir=.rsync-partial --backup --backup-dir="/Ameer/Phone.old/backup_$(date +%Y-%m-%d_%H.%M.%S)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@$host/usb1/Ameer/Phone
     status="$?"
     if [ "$status" != 0 ]; then
-      echo "An error occured during backup!"
-      $logger -p e "An error occured during backup!"
-      curl -s -X POST https://maker.ifttt.com/trigger/rsyc_event/with/key/$ifttt_key --data 'value1=An+error+occured+during+backup!' >/dev/null 2>&1
+      log "An error occured during backup!"
     else
-      echo "Backup successful!"
-      $logger -p i "Backup successful!"
-      curl -s -X POST https://maker.ifttt.com/trigger/rsync_event/with/key/$ifttt_key --data 'value1=Backup+successful!' >/dev/null 2>&1
+      log "Backup successful!"
     fi
   else
-    echo "USB is not mounted at remote!"
-    $logger -p e "USB is not mounted at remote!"
-    curl -s -X POST https://maker.ifttt.com/trigger/rsync_event/with/key/$ifttt_key --data 'value1=USB+is+not+mounted+at+remote!' >/dev/null 2>&1
+    log "USB is not mounted at remote!"
   fi
 fi
