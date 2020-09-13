f40c0ed1d891682b94a1ed523f90bf2af6810035
Ameer Dawood <ameer1234567890@gmail.com>
2020-09-13 22:06:10 +0500

    Fix log level on Termux

--- sync.sh
+++ sync.sh
@@ -3,15 +3,15 @@ host="192.168.100.71"
 logger="/system/bin/log -t sync"
 
 log() {
-  echo "$@"
-  $logger -p e "$@"
-  termux-notification --title 'rsync' --content "$@"
+  echo "$2"
+  $logger -p "$1" "$2"
+  termux-notification --title 'rsync' --content "$2"
 }
 
 ping -w 3 -c 1 $host >/dev/null 2>&1
 status="$?"
 if [ "$status" != 0 ]; then
-  log "Server unavailable!"
+  log "e" "Server unavailable!"
 else
   rsync rsync://$host/usb1/USB_NOT_MOUNTED >/dev/null 2>&1
   status="$?"
@@ -19,11 +19,11 @@ else
     rsync -ahv --copy-links --delete --progress --stats --partial --partial-dir=.rsync-partial --backup --backup-dir="/Ameer/Phone.old/backup_$(date +%Y-%m-%d_%H.%M.%S)" --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt /storage/emulated/0/ rsync://user@$host/usb1/Ameer/Phone
     status="$?"
     if [ "$status" != 0 ]; then
-      log "An error occured during backup!"
+      log "e" "An error occured during backup!"
     else
-      log "Backup successful!"
+      log "i" "Backup successful!"
     fi
   else
-    log "USB is not mounted at remote!"
+    log "e" "USB is not mounted at remote!"
   fi
 fi
