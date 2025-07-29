a66825cb45d95f7b7ead2a4d145e3ae2cac16098
Ameer Dawood <ameer1234567890@gmail.com>
2025-07-29 10:44:58 +0500

    Fixed syncin

--- syncin.sh
+++ syncin.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-host="192.168.100.128"
+host="nas1.lan"
 
 rsync rsync://$host/usb1/USB_NOT_MOUNTED >/dev/null 2>&1
 status="$?"
 if [ "$status" != 0 ]; then
-  rsync -rlpgohv --delete --progress --stats --partial --partial-dir=.rsync-partial --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt rsync://$host/usb1/Ameer/Phone/ /storage/emulated/0
+  rsync -ahv --delete --progress --stats --partial --partial-dir=.rsync-partial --exclude-from /storage/emulated/0/Ameer/rsync-excludes.txt rsync://$host/usb1/Ameer/Phone/ /storage/emulated/0
   status="$?"
   if [ "$status" != 0 ]; then
     termux-notification --title "Error!" --content "An error occured during restore!"
